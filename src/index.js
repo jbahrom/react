4b18d96e2c04d8bc1b4706f85c115d36c5021ef7
jbahrom <bahromjorayev33@gmail.com>
2021-08-03 23:48:48 +0300

    adsasd

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Student from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Student />
+    <Student />
+    <Student />
+
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
